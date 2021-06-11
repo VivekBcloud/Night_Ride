5f4d4b4d4bf6fd4c8cc7f1499ea3f4b6ac2796b5
VivekBcloud <20vivektoppo@gmail.com>
2021-06-11 15:22:57 +0530

    Ride��

---
+++ style.js
@@ -0,0 +1,9 @@
+document.querySelector("#carbody").addEventListener("click", () => {
+  document.querySelector("#carbody #Vector_11").classList.toggle("nighttheme");
+  document.querySelector("#carbody #Vector_12").classList.toggle("nighttheme");
+  document.querySelector("#carbody #Vector").classList.toggle("nighttheme");
+  document.querySelector("#carbody #Vector_13").classList.toggle("nightred");
+  document.querySelector("#carbody #Vector_14").classList.toggle("nightred");
+  document.querySelector("#carbody #Vector_5").classList.toggle("nightwhite");
+  document.querySelector("#light").classList.toggle("nightwhite");
+});
